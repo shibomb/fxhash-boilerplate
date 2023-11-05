0b57f411bee05deae0f971e7de9473f1a4694c0e
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-11-05 16:29:18 +0900

    Merge branch 'master' into p5js/master
    # Conflicts:
    #	README.md
    #	index.js
    #	project/public/index.html
    #	project/public/style.css

--- index.js
+++ index.js
@@ -90,83 +90,83 @@ $fx.features({
   "Feature from params, its a number": $fx.getParam("number_id"),
 })
 
-function main() {
-  // log the parameters, for debugging purposes, artists won't have to do that
-  // console.log("Current param values:");
-  // // Raw deserialize param values
-  // console.log($fx.getRawParams());
-  // // Added addtional transformation to the parameter for easier usage
-  // // e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
-  // console.log($fx.getParams());
+// function main() {
+//   // log the parameters, for debugging purposes, artists won't have to do that
+//   // console.log("Current param values:");
+//   // // Raw deserialize param values
+//   // console.log($fx.getRawParams());
+//   // // Added addtional transformation to the parameter for easier usage
+//   // // e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
+//   // console.log($fx.getParams());
 
-  // // how to read a single raw parameter
-  // console.log("Single raw value:");
-  // console.log($fx.getRawParam("color_id"));
-  // // how to read a single transformed parameter
-  // console.log("Single transformed value:");
-  // console.log($fx.getParam("color_id"));
+//   // // how to read a single raw parameter
+//   // console.log("Single raw value:");
+//   // console.log($fx.getRawParam("color_id"));
+//   // // how to read a single transformed parameter
+//   // console.log("Single transformed value:");
+//   // console.log($fx.getParam("color_id"));
 
-  const getContrastTextColor = backgroundColor =>
-    ((parseInt(backgroundColor, 16) >> 16) & 0xff) > 0xaa
-      ? "#000000"
-      : "#ffffff"
+//   const getContrastTextColor = backgroundColor =>
+//     ((parseInt(backgroundColor, 16) >> 16) & 0xff) > 0xaa
+//       ? "#000000"
+//       : "#ffffff"
 
-  const bgcolor = $fx.getParam("color_id").hex.rgba
-  const textcolor = getContrastTextColor(bgcolor.replace("#", ""))
+//   const bgcolor = $fx.getParam("color_id").hex.rgba
+//   const textcolor = getContrastTextColor(bgcolor.replace("#", ""))
 
-  // update the document based on the parameters
-  document.body.style.background = bgcolor
-  document.body.innerHTML = `
-  <div style="color: ${textcolor};">
-    <p>
-    hash: ${$fx.hash}
-    </p>
-    <p>
-    minter: ${$fx.minter}
-    </p>
-    <p>
-    iteration: ${$fx.iteration}
-    </p>
-    <p>
-    inputBytes: ${$fx.inputBytes}
-    </p>
-    <p>
-    context: ${$fx.context}
-    </p>
-    <p>
-    params:
-    </p>
-    <pre>
-    ${$fx.stringifyParams($fx.getRawParams())}
-    </pre>
-  <div>
-  `
-  const btn = document.createElement("button")
-  btn.textContent = "emit random params"
-  btn.addEventListener("click", () => {
-    $fx.emit("params:update", {
-      number_id: $fx.getRandomParam("number_id"),
-      bigint_id: $fx.getRandomParam("bigint_id"),
-      string_id_long: $fx.getRandomParam("string_id_long"),
-      select_id: $fx.getRandomParam("select_id"),
-      color_id: $fx.getRandomParam("color_id"),
-      boolean_id: $fx.getRandomParam("boolean_id"),
-      string_id: $fx.getRandomParam("string_id"),
-    })
-    main()
-  })
-  document.body.appendChild(btn)
-}
+//   // update the document based on the parameters
+//   document.body.style.background = bgcolor
+//   document.body.innerHTML = `
+//   <div style="color: ${textcolor};">
+//     <p>
+//     hash: ${$fx.hash}
+//     </p>
+//     <p>
+//     minter: ${$fx.minter}
+//     </p>
+//     <p>
+//     iteration: ${$fx.iteration}
+//     </p>
+//     <p>
+//     inputBytes: ${$fx.inputBytes}
+//     </p>
+//     <p>
+//     context: ${$fx.context}
+//     </p>
+//     <p>
+//     params:
+//     </p>
+//     <pre>
+//     ${$fx.stringifyParams($fx.getRawParams())}
+//     </pre>
+//   <div>
+//   `
+//   const btn = document.createElement("button")
+//   btn.textContent = "emit random params"
+//   btn.addEventListener("click", () => {
+//     $fx.emit("params:update", {
+//       number_id: $fx.getRandomParam("number_id"),
+//       bigint_id: $fx.getRandomParam("bigint_id"),
+//       string_id_long: $fx.getRandomParam("string_id_long"),
+//       select_id: $fx.getRandomParam("select_id"),
+//       color_id: $fx.getRandomParam("color_id"),
+//       boolean_id: $fx.getRandomParam("boolean_id"),
+//       string_id: $fx.getRandomParam("string_id"),
+//     })
+//     main()
+//   })
+//   document.body.appendChild(btn)
+// }
 
-main()
+// main()
 
-$fx.on(
-  "params:update",
-  newRawValues => {
-    // opt-out default behaviour
-    if (newRawValues.number_id === 5) return false
-    // opt-in default behaviour
-    return true
-  },
-  (optInDefault, newValues) => main()
-)
+// $fx.on(
+//   "params:update",
+//   newRawValues => {
+//     // opt-out default behaviour
+//     if (newRawValues.number_id === 5) return false
+//     // opt-in default behaviour
+//     return true
+//   },
+//   (optInDefault, newValues) => main()
+// )
---
+++ p5js/sketch.js
@@ -0,0 +1,78 @@
+function setup() {
+  // console.log("setup()")
+
+  createCanvas(windowWidth, windowHeight);
+
+  const bgcolor = $fx.getParam("color_id").hex.rgba
+  background(bgcolor);
+
+  //
+  drawFxRandomButton()
+}
+
+function draw() {
+  // console.log("draw()")
+
+  const bgcolor = $fx.getParam("color_id").hex.rgba
+  background(bgcolor);
+
+  //
+  drawFxValues()
+}
+
+function drawFxValues() {
+  const getContrastTextColor = backgroundColor =>
+    ((parseInt(backgroundColor, 16) >> 16) & 0xff) > 0xaa
+      ? "#000000"
+      : "#ffffff"
+
+  const bgcolor = $fx.getParam("color_id").hex.rgba
+  const textcolor = getContrastTextColor(bgcolor.replace("#", ""))
+
+  noStroke()
+  fill(textcolor)
+
+  const valueTexts = [
+    `hash: ${$fx.hash}`,
+    `minter: ${$fx.minter}`,
+    `iteration: ${$fx.iteration}`,
+    `inputBytes: ${$fx.inputBytes}`,
+    `context: ${$fx.context}`,
+    `params:${$fx.stringifyParams($fx.getRawParams())}`
+  ]
+
+  for(let i = 0; i < valueTexts.length; i++) {
+    text(valueTexts[i], 10, i * 20 + 60);
+  }
+}
+
+let btn;
+function drawFxRandomButton() {
+  btn = createButton("emit random params")
+  btn.position(10, 10);
+  btn.mousePressed(() => {
+    // console.log("emit params:update")
+    $fx.emit("params:update", {
+      number_id: $fx.getRandomParam("number_id"),
+      bigint_id: $fx.getRandomParam("bigint_id"),
+      string_id_long: $fx.getRandomParam("string_id_long"),
+      select_id: $fx.getRandomParam("select_id"),
+      color_id: $fx.getRandomParam("color_id"),
+      boolean_id: $fx.getRandomParam("boolean_id"),
+      string_id: $fx.getRandomParam("string_id"),
+    })
+  })
+}
+
+$fx.on(
+  "params:update",
+  newRawValues => {
+    // opt-out default behaviour
+    if (newRawValues.number_id === 5) return false
+    // opt-in default behaviour
+    return true
+  },
+  (optInDefault, newValues) => {
+    // console.log("on params:update", [optInDefault, newValues])
+  }
+)
